aaa9f220b71778096ac0fcc323c5f216b875e379
Diego Alencar Jacober <57761933898>
2023-09-11 13:52:46 -0300

    Configuring next auth

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import style from './style.module.css';
+
+
+export default function Dashboard() {
+    return(
+        <div className={style.container}>
+                meu painel
+        </div>
+    )
+}
