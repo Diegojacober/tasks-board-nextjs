1c7171064a3e43f46494018b2d44baf6b9f1a342
Diego Alencar Jacober <57761933898>
2023-09-12 14:27:31 -0300

    Creating static page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,16 @@ import Head from 'next/head';
 import styles from '@/styles/home.module.css';
 import Image from 'next/image';
 import heroImg from '@/../public/assets/hero.png';
+import { GetStaticProps } from 'next';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '@/services/firebaseConnection';
 
-export default function Home() {
+interface HomeProps {
+  posts: number,
+  comments: number
+}
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
     <>
       <Head>
@@ -23,14 +31,31 @@ export default function Home() {
 
         <div className={styles.infoContent}>
           <section className={styles.box}>
-            <span>+12 posts</span>
+            <span>+{posts} posts</span>
           </section>
 
           <section className={styles.box}>
-            <span>+90 comentários</span>
+            <span>+{comments} comentários</span>
           </section>
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const commentRef = collection(db, "comments")
+  const commentSnapshot = await getDocs(commentRef)
+
+  const postRef = collection(db, "tarefas")
+  const postSnapshot = await getDocs(postRef)
+
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0
+    },
+    revalidate: 60, //revalidada a cada 60 segundos, poupando requisições
+  }
 }
